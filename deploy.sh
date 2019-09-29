051fe59852f21076e9f8d75e98fbffc99f0d6e7e
madden1706@gmail.com <madden1706@gmail.com>
2019-09-29 22:06:57 +0100

    Add list of TODOs.

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,19 @@ heroku git:remote -a $1
 
 heroku stack:set container -a $1
 
+heroku config:set DISABLE_COLLECTSTATIC=0
+
 # Push the master branch to heroku
 # OR git push heroku testbranch:master
  git push heroku $2:master
+
+# TODO to update the envs with heroku config:set
+# TODO to update the debug setting. 
+# TODO update allowed hosts.
+# TODO script to back up DB.
+# TODO script to restore a DB 
+# Install black as a pre-commit hook.
+
 
 # To destroy app
 # heroku apps:destroy $1--confirm $1
